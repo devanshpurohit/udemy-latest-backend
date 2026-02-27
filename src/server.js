1e1c666a73792703435afac573a32a3020caeaa3
Devansh <purohitdevansh@gmail.com>
2026-02-27 11:19:08 +0530

    hgd

--- src/server.js
+++ src/server.js
@@ -32,12 +32,8 @@ app.use(helmet());
 ======================= */
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'production' 
-      ? ['https://udemy-admin-frontend-5r5w.vercel.app', 'https://udemy-website-frontend.vercel.app', 'https://yourdomain.com', 'https://www.yourdomain.com'] 
-      : ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    origin: true,   // request ka origin allow karega
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
